993216eb6fcd49dea88aacc313779dac8f881ace
javiergael61-ux <javiergael61@gmail.com>
2025-08-02 16:40:55 -0500

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,71 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "Me gustan tus ojos azul", time:0 },
+  { text: "Me encanta tu pelo que es como una nube", time: 3 },
+  { text: "que ha llovido tanto que no tiene nada, mas que ocultar", time: 4 },
+  { text: "Me encanta tu boca, que dice que sabe entiende, perdona los sabios y giles.", time: 8 },
+  { text: "que han comido tanto, que han hagotado su voluntad de querer", time: 12 },
+  { text: "Me gusta que uses la palabra amigo", time: 15 },
+  { text: "cuando quiero estar un rato contigo", time: 17 },
+  { text: "no hay ningun secreto que guarde conmigo, sin preguntar", time: 20 },
+  { text: "Me encanta sentir asi tu respiro, habriendose paso al invirno tan frio", time: 25 },
+  { text: "El mundo gigante, cambiante y jodido, en bueno sentirse asi", time: 28 },
+  { text: "Me encanta esa manera en ti", time: 32 },
+  { text: "I only want to live in clouds", time: 83 },
+  { text: "Where I'm now? I don't know why", time: 91 },
+  { text: "Nice butterflies in my hands", time: 97 },
+  { text: "Too much light for twilight", time: 104 },
+  { text: "In the mood for the flowers love", time: 108 },
+  { text: "At the time", time: 144 },
+  { text: "The whisper of birds", time: 148 },
+  { text: "Lonely before the sun cried", time: 153 },
+  { text: "Fell from the sky", time: 158 },
+  { text: "Like water drops", time: 164 },
+  { text: "Where I'm now? I don't know why", time: 169 },
+  { text: "Nice butterflies in my hands", time: 176 },
+  { text: "Too much light for twilight", time: 183 },
+  { text: "In the mood for the flowers", time: 188 },
+  { text: "Love.", time: 140 },
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
